d613170c25f572643efcfe0522a4a728c04aa277
Tarl Hahn <thahn420@uw.edu>
2017-11-20 00:10:38 -0800

    implemented dp solution to the coin change problem

--- src/main/java/cracking/coding/interview/dpcoinchange/DpCoinChange.java
+++ src/main/java/cracking/coding/interview/dpcoinchange/DpCoinChange.java
@@ -17,7 +17,21 @@ public class DpCoinChange {
 	 * @param values The array of values
 	 * @return The number of ways to sum values that equal target.
 	 */
-	public int waysToMakeChange(int target, int[] values) {
-		return 0;
+	public long waysToMakeChange(int target, int[] values) {
+		
+        long[] ways = new long[target + 1];
+ 
+        // base case
+        ways[0] = 1;
+ 
+        // one by one update the table
+        for (int i = 0; i < values.length; i++) {
+            
+        	// accumulate the solutions
+        	for (int j = values[i]; j <= target; j++) {
+                ways[j] += ways[j - values[i]];
+            }
+        }
+        return ways[target];
 	}
 }
